39a81efd1e28c3f843379e609d1df1e23ab7c295
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-10-07 16:52:09 +0300

    add modal and control panel functional

--- task2/index.js
+++ task2/index.js
@@ -1,13 +1,66 @@
 import images from './data.js';
-import {galleryItemTemplate} from './templates.js';
+import {galleryItemTemplate, modalTemplate} from './templates.js';
 
 const galleryContainer = document.querySelector('.gallery');
 
-galleryContainer.innerHTML = '';
+const updateAmount = () => {
+    const itemCollection = document.querySelectorAll('.gallery-item');
+    document.querySelector('.amount').innerHTML = itemCollection.length + ''
+}
 
-images.forEach((img) => {
-    galleryContainer.innerHTML += galleryItemTemplate(img);
-})
+const addItems = () => {
+    galleryContainer.innerHTML = '';
+    images.forEach((img) => {
+        galleryContainer.innerHTML += galleryItemTemplate(img);
+    })
+    updateAmount();
+}
+
+addItems()
+
+const setDate = () => {
+    const dateControlElement = document.querySelector('.date');
+    const date = new Date();
+
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+    dateControlElement.innerHTML = `${day}.${month}.${year} ${hours}:${minutes}`;
+}
+
+
+(() => {
+    setDate()
+    setInterval(setDate, 60_000)
+})()
+
+
+const openModal = (e) => {
+    const target = e.target;
+    if (target.getAttribute('data-id')) {
+        const id = target.dataset.id;
+
+        if (id) {
+            const image = images.find(image => image.id === id)
+            if (image) {
+                const modal = document.createElement('div');
+                modal.innerHTML = modalTemplate(image.src);
+
+                document.body.appendChild(modal)
+                const closeModalButton = modal.querySelector('.close');
+
+                closeModalButton.addEventListener('click', () => {
+                    modal.remove()
+                })
+            }
+        }
+    }
+}
+
+galleryContainer.addEventListener('click', openModal)
 
 
 
--- task2/templates.js
+++ task2/templates.js
@@ -2,6 +2,21 @@
 
 export const galleryItemTemplate  = (image) => {
     return `<div class="gallery-item">
-        <div class="background" style="background-image: url('${image.src}')"></div>
+        <div class="background" 
+        style="background-image: url('${image.src}')" 
+        data-id="${image.id}"></div>
+    </div>`
+}
+
+export const modalTemplate = (imageSrc) => {
+    return `<div class="modal">
+        <div class="content">
+            <div class="image">
+                <img src="${imageSrc}" alt=""/>
+            </div>
+            <div class="close">
+                <img src="./images/close.svg" alt="close"/>
+            </div>
+        </div>
     </div>`
 }
